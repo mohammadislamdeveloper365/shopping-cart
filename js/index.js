1154e01a85e4dc422d1356a1bba784a10d3a56a1
Mohammad Moinul Islam <95491483+mohammadislamdeveloper365@users.noreply.github.com>
2023-11-10 20:58:40 +0400

    fix: styling and code optimization

--- js/index.js
+++ js/index.js
@@ -8,9 +8,10 @@ function changeNumber(id,isIncrement) {
     }
 
     else {
-        --numberOfTv;
-        if(numberOfTv >= 0)
-        tvNumberField.value = numberOfTv;
+        if(numberOfTv > 0) {
+            --numberOfTv;
+            tvNumberField.value = numberOfTv;
+        }
     }
 }
 
@@ -21,41 +22,40 @@ function setTotalPrice(itemPrice, itemNumberId, itemPriceId) {
     itemField.innerText = price * numberOfItem;
 }
 
-function setPrice (tvPriceId, tvStandPriceId) {
-    let tvPrice = getNumberValue(tvPriceId);
-    let tvStandPrice = getNumberValue(tvStandPriceId);
-    let subTotalField = getElement('sub-total');
+function setPrice (tvPriceId, tvStandPriceId, vatId, subTotalId, totalId) {
+    const subTotalField = getElement(subTotalId);
+    const totalField = getElement(totalId);
+    const tvPrice = getNumberValue(tvPriceId);
+    const tvStandPrice = getNumberValue(tvStandPriceId);
+    const vatField = getElement(vatId);
+    
     subTotalField.innerText = tvPrice + tvStandPrice;
-
-    let vatField = getElement('vat');
     vatField.innerText = (0.06 * (tvPrice + tvStandPrice)).toFixed(2);
-
-    let totalField = getElement('total');
-    totalField.innerText = getNumberValue('sub-total') + getNumberValue('vat')
+    totalField.innerText = getNumberValue(subTotalId) + getNumberValue(vatId)
 }
 
-setPrice('tv-price','tv-stand-price');
+setPrice('tv-price','tv-stand-price','vat','sub-total','total');
 
 getElement("btn-tv-plus").addEventListener('click', function() {
     changeNumber('tv-number',true);
     setTotalPrice(450, 'tv-number', 'tv-price');
-    setPrice('tv-price','tv-stand-price');
+    setPrice('tv-price','tv-stand-price','vat','sub-total','total');
 })
 
 getElement("btn-tv-minus").addEventListener('click', function() {
    changeNumber('tv-number',false);
    setTotalPrice(450, 'tv-number', 'tv-price');
-   setPrice('tv-price','tv-stand-price')
+   setPrice('tv-price','tv-stand-price', 'vat','sub-total','total')
 })
 
 getElement("btn-tv-stand-plus").addEventListener('click', function() {
     changeNumber('tv-stand-number',true);
     setTotalPrice(20, 'tv-stand-number','tv-stand-price');
-    setPrice('tv-price','tv-stand-price');
+    setPrice('tv-price','tv-stand-price', 'vat','sub-total','total');
 })
 
 getElement("btn-tv-stand-minus").addEventListener('click', function() {
    changeNumber('tv-stand-number',false);
    setTotalPrice(20, 'tv-stand-number','tv-stand-price');
-   setPrice('tv-price','tv-stand-price');
+   setPrice('tv-price','tv-stand-price','vat','sub-total','total');
 })
